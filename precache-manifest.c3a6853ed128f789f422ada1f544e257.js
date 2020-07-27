81b956e68e59bcb0e9b96147d6a0e8d0a59704c0
vladshatilov <shatilovvlad@mail.ru>
2020-07-27 18:05:52 +0300

    Updates

---
+++ precache-manifest.c3a6853ed128f789f422ada1f544e257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9709aff0bf2c3ac688b0a1da7d60d441",
+    "url": "/react_sortable_Table/index.html"
+  },
+  {
+    "revision": "2eb0ad4942f85a97cb56",
+    "url": "/react_sortable_Table/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "db0bebc607c9ad06f325",
+    "url": "/react_sortable_Table/static/css/main.cdea875f.chunk.css"
+  },
+  {
+    "revision": "2eb0ad4942f85a97cb56",
+    "url": "/react_sortable_Table/static/js/2.5832fcda.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/react_sortable_Table/static/js/2.5832fcda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0bebc607c9ad06f325",
+    "url": "/react_sortable_Table/static/js/main.a6191943.chunk.js"
+  },
+  {
+    "revision": "025a134aef4c98fc4568",
+    "url": "/react_sortable_Table/static/js/runtime-main.e4285590.js"
+  }
+]);
